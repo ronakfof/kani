dae29a4c927236e218fb013485f8227da03194da
celinval <celinval@amazon.com>
2021-11-10 20:26:19 +0000

    Deploying to gh-pages from @ model-checking/rmc@2141a76f98dbe3e339c6a1e184ac778965f2da54 🚀

--- dashboard/artifacts/The Unstable Book/Library Features/global_asm/77.rs
+++ dashboard/artifacts/The Unstable Book/Library Features/global_asm/77.rs
@@ -1,6 +1,6 @@
 // compile-flags: --edition 2018
 #![allow(unused)]
-#![feature(global_asm)]
+#![feature(global_asm, asm_const)]
 pub fn main() {
 #[cfg(any(target_arch="x86", target_arch="x86_64"))]
 mod x86 {
