e870cbb7e9f091f93cfd595439a1dcbc409af8c8
celinval <celinval@amazon.com>
2021-12-29 20:50:19 +0000

    Deploying to gh-pages from @ model-checking/rmc@414dca40e6be76ce0e323ee5a66db756bee58b63 🚀

--- dashboard/artifacts/The Unstable Book/Library Features/asm/195.rs
+++
@@ -1,9 +0,0 @@
-// compile-flags: --edition 2015
-#![allow(unused)]
-#![feature(asm)]
-fn main() {
-let cmd = 0xd1;
-unsafe {
-    asm!("out 0x64, eax", in("eax") cmd);
-}
-}
--- dashboard/artifacts/The Unstable Book/Library Features/asm/41.rs
+++
@@ -1,8 +0,0 @@
-// compile-flags: --edition 2015
-#![allow(unused)]
-#![feature(asm)]
-fn main() {
-unsafe {
-    asm!("nop");
-}
-}
